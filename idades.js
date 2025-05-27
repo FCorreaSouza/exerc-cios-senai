6476b3b8545366869f163507fd4a7dd6e542722a
FCorreaSouza <fernando_c_souza@estudante.sesisenai.org.br>
2025-05-27 17:07:49 -0300

    Add files via upload
    primeiro programa

---
+++ idades.js
@@ -0,0 +1,11 @@
+
+// ler a idade 
+//se idade 18 ou mais falar q é de maior
+//senão falar q é menor
+
+let idade = prompt("Qual é a sua idade? ")
+if(idade >= 18){
+alert("Você é de maior")
+}else{
+alert("Você é de menor")
+}
