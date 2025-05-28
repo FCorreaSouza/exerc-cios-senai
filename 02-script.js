414806c5cd1355c34b5256051ded4fdf95bcf3b7
FCorreaSouza <fernando_c_souza@estudante.sesisenai.org.br>
2025-05-28 17:08:07 -0300

    Add files via upload

---
+++ 02-script.js
@@ -0,0 +1,17 @@
+//sq2   ex13  
+
+let idade 
+
+idade = prompt("Digite a sua idade: ")
+if(idade >= 16 ){
+  alert("Você pode votar :) ")
+}else{
+  alert("Você NÃO pode votar :( ")
+    
+
+
+}
+
+
+
+
