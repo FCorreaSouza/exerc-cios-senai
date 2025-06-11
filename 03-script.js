c162f1464d1ea9e38366d379ac51803b17a159bd
FCorreaSouza <fernando_c_souza@estudante.sesisenai.org.br>
2025-06-11 16:49:51 -0300

    Add files via upload

---
+++ 03-script.js
@@ -0,0 +1,14 @@
+//ler a idade do cachorro e calcular ela 
+
+
+
+let idade = Number(prompt("Qual a idade do sue cachorro? " ))
+let idadeDecachorro = idade * 7 
+alert("em idade de cachorro, isso da " + idadeDecachorro + " anos")
+
+if(idadeDecachorro >= 65){
+   alert("ele ja pode se aposentar")
+}else{
+ alert("ele não pode se aposentra ainda.")
+
+}
---
+++ 04-script.js
@@ -0,0 +1,22 @@
+let palestra = Number (prompt("Digite o Número da palestra que você queira ver:"))
+if(palestra ==1){
+   alert("Foi escolida a palestra Animações com Scratch, Lab 305 as 19H ") 
+   
+ } if(palestra ==2){
+    alert("Foi escolida a palestra scratch para gamers, Lab 512, 20H ")
+
+ } if(palestra ==3){
+    alert("Foi escolida a palestra JavaScript para Leigos, Lab 101, 19H ")
+
+ } if(palestra ==4){
+    alert("Foi escolida a palestra Topicos avançados de JavaScript, Lab 305, 20H ")
+
+ } if(palestra ==5){
+    alert("Foi escolida a palestra Vida e carreira, Auditório, 21H ")
+
+ } else {
+   alert("Digite um Número valido")
+
+ }
+
+ 
