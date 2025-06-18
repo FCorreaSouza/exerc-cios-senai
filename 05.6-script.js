71cb255ed67fdcca8a0573b3f153b567afb54854
FCorreaSouza <fernando_c_souza@estudante.sesisenai.org.br>
2025-06-18 15:30:21 -0300

    Add files via upload

---
+++ 05.6-script.js
@@ -0,0 +1,11 @@
+let nota1 = Number(prompt("Digite a sua primeira nota: "))
+let nota2 = Number(prompt("Digite a sua segunda nota: "))
+
+let media = (nota1v + nota2) / 2
+
+if(media >= 7){
+
+  alert("passou, media: " + media )
+}else{
+  alert("Não deu, Media: " + media ) 
+}
